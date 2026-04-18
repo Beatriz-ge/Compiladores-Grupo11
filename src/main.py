29dc2cbb8a4ab86f7b0de26ffeb8e04c82a25281
isabellachoukaira <i.choukaira@gmail.com>
2026-04-17 21:09:25 -0300

    feat: implementa debug de tokens

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 from lexer.lexer import Lexer
 from parser.parser import Parser
 
+def debug_tokens(code):
+    from lexer.lexer import Lexer
+    lexer = Lexer(code)
+    print("\n" + "="*20)
+    print(" DEBUG DE TOKENS")
+    print("="*20)
+    
+    while True:
+        token = lexer.get_next_token()
+        print(token)
+        if token.type.name == 'EOF': # Verifique se o seu fim de arquivo se chama EOF
+            break
+            
+    print("="*20 + "\n")
+
 def main():
     code = """
     int main() {
@@ -8,8 +23,11 @@ def main():
         return x;
     }
     """
+    debug_tokens(code)
+
     parser = Parser(Lexer(code))
-    result = parser.parse_program() 
+    result = parser.parse_program()
+    print("AST GERADA:")
     print(result)
 
 
